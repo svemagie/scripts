c83b5d6f7a3e56958bbc84e088bdb786e2ae6350
Sven <sven@giersig.eu>
2020-01-13 11:33:23 +0100

    del bash

--- archive-notes-tag-counter.bash
+++
@@ -1,6 +0,0 @@
-# Simple Bash Script to Search a Directory of Notes with Tags 
-TARGET_TAG=${1?Error: no tag given}
-# TARGET_TAG="InsightNote"
-cd /Users/sven/Documents/Projekte/Notes\ \(The\ Archive\)
-count=$(grep -wl "#$TARGET_TAG" * | wc -l | tr -d '[:space:]')
-echo "$count notes with #$TARGET_TAG" 
