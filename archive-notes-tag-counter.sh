34dc03861c215dd181a7d4383a50e1453abbd41b
Sven Giersig <svemagie@noreply.chibatsu.net>
2021-01-07 14:10:58 +0100

    change notes path

--- archive-notes-tag-counter.sh
+++ archive-notes-tag-counter.sh
@@ -1,6 +1,6 @@
 # Simple Bash Script to Search a Directory of Notes with Tags 
 TARGET_TAG=${1?Error: no tag given}
 # TARGET_TAG="InsightNote"
-cd /Users/sven/Documents/Projekte/Notes\ \(The\ Archive\)
+cd /Users/sven/Documents/Projekte/notes
 count=$(grep -wl "#$TARGET_TAG" * | wc -l | tr -d '[:space:]')
 echo "$count notes with #$TARGET_TAG" 
